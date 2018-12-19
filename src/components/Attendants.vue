2a4de182d9b2aaabb5b9602063052523cf1f02cc
Korver <korver2017@gmail.com>
2018-12-19 16:00:37 +0800

    set default attendants data

--- src/components/Attendants.vue
+++ src/components/Attendants.vue
@@ -2,14 +2,13 @@
   <div class="w-100">
     <h1>Hello Attendants!</h1>
     <ul class="d-flex flex-direction-row flex-wrap">
-      <!-- <li v-for="(value, key) in attObj" :key="key">{{ value }}</li> -->
-      <!-- <slot>
-        <app-attendant :attObj.sync="attObj" :nameList="attendants" :imgs="imgs"></app-attendant>
-      </slot>-->
-      <li ref="list" class="avatar col-3" v-for="(value, key) in attendants" :key="key">
+      <li class="avatar col-3" v-for="(value, key) in attendants.name" :key="key">
+        {{ key }} | {{ value }}
+      </li>
+      <!-- <li ref="list" class="avatar col-3" v-for="(value, key) in attendants" :key="key">
         {{ key }}
         <img :src="value" alt>
-      </li>
+      </li> -->
     </ul>
   </div>
 </template>
@@ -18,6 +17,7 @@
 import appAttendant from "./Attendant";
 import appLottery from "./Lottery";
 import { eventBus } from "../main";
+import { defaultAttendants } from "../data";
 
 export default {
   components: {
@@ -26,33 +26,39 @@ export default {
   },
   data() {
     return {
-      attendants: {},
+      attendants: {
+        departments: ['Front-End Developer', 'UI/UX Designer'],
+        name: {},
+      },
     }
   },
   created() {
-    this.callAPI();
+    defaultAttendants.forEach(item => {
+      this.$set(this.attendants.name, item.name, item.department);
+    });
+    console.log(this.attendants.name);
   },
   methods: {
-    callAPI() {
-      let vm = this;
-      fetch("https://randomuser.me/api/?results=500&gender=female", {})
-        .then(response => {
-          console.log(response);
-          return response.json();
-        })
-        .then(jsonData => {
-          var res = jsonData.results;
-          for (let i = 0; i < 20; i++) {
-            vm.attendants[`${res[i].name.first} ${res[i].name.last}`] =
-              res[i].picture.large;
-          }
-          console.log(vm.attendants);
-          vm.$set(this.attendants, vm.attendants);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
+    // callAPI() {
+    //   let vm = this;
+    //   fetch("https://randomuser.me/api/?results=500&gender=female", {})
+    //     .then(response => {
+    //       console.log(response);
+    //       return response.json();
+    //     })
+    //     .then(jsonData => {
+    //       var res = jsonData.results;
+    //       for (let i = 0; i < 20; i++) {
+    //         vm.attendants[`${res[i].name.first} ${res[i].name.last}`] =
+    //           res[i].picture.large;
+    //       }
+    //       console.log(vm.attendants);
+    //       vm.$set(this.attendants, vm.attendants);
+    //     })
+    //     .catch(err => {
+    //       console.log(err);
+    //     });
+    // }
   }
 };
 </script>
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const defaultAttendants = [
+  {
+    department: 'Front-End Developer',
+    name: 'Korver',
+    img: ''
+  },
+  {
+    department: 'UI/UX Designer',
+    name: 'Sara',
+    img: ''
+  }
+];
